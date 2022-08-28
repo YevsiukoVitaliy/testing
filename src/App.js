77b6912486d536e95b3fd219e8a5915ac3455964
YevsiukoVitaliy <93053290+YevsiukoVitaliy@users.noreply.github.com>
2022-08-28 04:35:45 +0300

    e

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useRef } from "react";
+
+const Player = ({ source }) => {
+  const playerRef = useRef();
+  const play = () => playerRef.current.play();
+  const pause = () => playerRef.current.pause();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <video ref={playerRef} src={source}>
+        Sorry, your browser does not support embedded videos.
+      </video>
+      <div>
+        <button onClick={play}>Play</button>
+        <button onClick={pause}>Pause</button>
+      </div>
     </div>
   );
-}
+};
+
+const App = () => {
+  return <Player source="http://media.w3.org/2010/05/sintel/trailer.mp4" />;
+};
 
 export default App;
